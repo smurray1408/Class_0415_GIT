eb48685e0040cf0da390c22d1090e5d2bd89e9a2
smurray1408 <scott.w.murray@comcast.net>
2015-05-19 08:01:18 -0700

    why isn't this working

--- Class_0415/src/TestLinkedList.java
+++ Class_0415/src/TestLinkedList.java
@@ -26,7 +26,7 @@ public class TestLinkedList {
 		System.out.println(testList);
 		System.out.println("Length of linked list = " + testList.length()); //expect 3		
 		
-		System.out.println("6! = " + testList.factorial(5));
+		System.out.println("5! = " + testList.factorial(5));
 		System.out.println("10! = " + testList.factorial(12));
 		
 		
